422265170171d41f0162cba968af47d03eac07ab
NikaKlokava <veronika.kazakevich1997@gmail.com>
2023-03-14 14:31:13 +0200

    remove files

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express"); // подключаем библиотеку экспресс
 const bodyParser = require("body-parser"); // импортируем библиотеку
+const MongoClient = require("mongodb").MongoClient; // импортировали MangoClient
 
 const app = express(); // это наш веб-сервер
+let db; //описываем переменную, чтобы она была доступна во всем файле
+// и в ней мы будем хранить переменную db которая будет ссылкой на БД после подключения
 
 app.use(bodyParser.json()); // эта строчка нужна чтобы правильно парсить json
 app.use(bodyParser.urlencoded({ extended: true })); // эта строчка нужна чтобы правильно парсить формы
@@ -36,7 +39,8 @@ app.get("/artists/:id", (req, res) => {
   res.send(artist);
 });
 
-app.post("/artists", (req, res) => { // добавить новый объект
+app.post("/artists", (req, res) => {
+  // добавить новый объект
   const artist = {
     id: Date.now(),
     name: req.body.name, // через постмэн отправляем json name
@@ -45,22 +49,41 @@ app.post("/artists", (req, res) => { // добавить новый объект
   res.send(artist);
 });
 
-app.put("/artists/:id", (req, res) => { // обновление данных, изменение имени объекта
+app.put("/artists/:id", (req, res) => {
+  // обновление данных, изменение имени объекта
   const artist = artists.find((artist) => {
     return artist.id === Number(req.params.id);
   });
   artist.name = req.body.name;
-//   res.send(artist)
+  //   res.send(artist)
   res.sendStatus(200);
 });
 
 app.delete("/artists/:id", (req, res) => {
+  // удалить артиста с данным id
   artists = artists.filter((artist) => {
     return artist.id !== Number(req.params.id);
   });
   res.sendStatus(200);
 });
 
-app.listen(3012, () => {
-  console.log("API app started");
-});
+// app.listen(3012, () => {
+//   console.log("API app started");
+// });
+
+const run = async () => {
+  console.log("Try to connect mongodb");
+  try {
+    const client = await MongoClient.connect("mongodb://127.0.0.1:27017");
+    console.log("Client is created! Try to create DB.");
+    db = client.db("myapi"); // это ссылка на нашу БД
+    console.log("DB is created! Start app listener");
+    app.listen(3012, () => {
+      console.log("API app started");
+    });
+  } catch (err) {
+    console.error("Error!", err);
+  }
+};
+
+run();
